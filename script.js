9ed2ca5646c3808ccb5b72a63759f31494194873
ebeng <ebengabraham2003@gmail.com>
2024-08-13 09:53:46 +0530

    day 1

---
+++ script.js
@@ -0,0 +1,21 @@
+function Check() {
+
+    let fname = document.getElementById("fname").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let pwd = document.getElementById("pwd").value.trim();
+    let msg = document.getElementById("msg");
+    msg.style.color=red;
+    msg.textContent = "";
+    if (fname === "") {
+        msg.textContent = "Enter Name";
+        return false;
+    } else if (email === "") {
+        msg.textContent = "Enter Email";
+        return false;
+    } else if (pwd === "") {
+        msg.textContent = "Enter Password";
+        return false;
+    }
+    return true;
+    
+}
